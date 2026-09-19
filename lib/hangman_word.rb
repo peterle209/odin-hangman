0d5920ac3b4e9b517544c4f4fb4da22998b4ded1
sinec1 <187554295+sinec1@users.noreply.github.com>
2026-09-19 15:37:19 -0700

    Implement logic to choose random(valid) word from dict.txt

--- lib/hangman_word.rb
+++ lib/hangman_word.rb
@@ -1,3 +1,23 @@
 class HangmanWord
+  MIN_LENGTH = 5
+  MAX_LENGTH = 12
+  POSSIBLEWORDS = 9894
 
+  def initialize
+    @word = get_word
+  end
+
+  def get_word
+    word = nil
+    until check_valid word
+      random_idx = rand(POSSIBLEWORDS)
+      word = File.readlines('dict.txt')[random_idx]
+    end
+    word
+  end
+
+  def check_valid(word)
+    return true if !word.nil? && word.size >= MIN_LENGTH && word.size <= MAX_LENGTH
+    false
+  end
 end
